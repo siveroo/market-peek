3402c840718b2dc595619a2d77edf76e67139fac
Dwi Andhika <53227252+siveroo@users.noreply.github.com>
2022-05-29 18:34:02 +0700

    rewrote fetchMarketItems code
    (hardcoded the query, will fix later)

--- src/func/fetchMarketItems.ts
+++ src/func/fetchMarketItems.ts
@@ -2,69 +2,72 @@ import { ParserConfig } from "../parser";
 import urlcat from "urlcat";
 import { delay, requestURL } from "./util";
 import { SearchResponse } from "../interface/search";
-import { insertItem, retrieveItemByName, upsertApp } from "../database";
+import { retrieveItemsByApp, upsertApp, upsertItem } from "../database";
 import { createMarketApp } from "../entity/marketApp";
-import { createMarketItem, MarketItem } from "../entity/marketItem";
-
-const COUNT_PER_REQUEST = 50;
-
-export async function fetchMarketItems(config: ParserConfig, filter: object, limit = Infinity) {
-    const ITEM_RETRIEVAL_DELAY = 15000;
-    const items: MarketItem[] = [];
-
+import { createMarketItem } from "../entity/marketItem";
+// https://steamcommunity.com/market/search?category_730_ItemSet%5B0%5D=any&category_730_ProPlayer%5B0%5D=any&category_730_StickerCapsule%5B0%5D=any&category_730_TournamentTeam%5B0%5D=any&category_730_Weapon%5B0%5D=any&category_730_Type%5B0%5D=tag_CSGO_Type_WeaponCase&category_730_Type%5B1%5D=tag_CSGO_Tool_Sticker&category_730_Type%5B2%5D=tag_CSGO_Type_Spray&appid=730
+export async function fetchMarketItems(config: ParserConfig, filter: object) {
     const baseUrl = `https://steamcommunity.com/market/search/render/`;
-    let params = {
+    const param = {
         l: config.language,
         currency: config.currency,
         appid: config.appid,
         norender: 1,
+        "category_730_Type[0]": "tag_CSGO_Type_WeaponCase",
+        "category_730_Type[1]": "tag_CSGO_Tool_Sticker",
+        "category_730_Type[2]": "tag_CSGO_Type_Spray",
         ...filter,
     };
+    const initialRequestUrl = urlcat(baseUrl, param);
+    const initialResponse = (await requestURL(initialRequestUrl, "json")) as SearchResponse | null;
+    const itemCount = initialResponse?.total_count;
 
-    const itemCountLimit = Math.min(limit, await getTotalItems(baseUrl, params));
-    for (let i = 0; i < itemCountLimit; i += COUNT_PER_REQUEST) {
+    if (initialResponse === null || !(itemCount > 0)) return;
+
+    const app = createMarketApp(initialResponse.results[0]);
+    const upsertedApp = await upsertApp(app);
+
+    if (upsertedApp === null) return;
+
+    const storedItemsName = (await retrieveItemsByApp(app)).map((item) => item.name);
+    const batchSize = 50;
+
+    for (let i = 0; i < itemCount; i += batchSize) {
         const page = {
             start: i,
-            count: COUNT_PER_REQUEST,
+            count: batchSize,
         };
 
-        params = { ...params, ...page };
+        const itemRequestUrl = urlcat(baseUrl, { ...param, ...page });
+        const response = (await requestURL(itemRequestUrl, "json")) as SearchResponse;
+        const itemEntries = response.results.entries();
 
-        const url = urlcat(baseUrl, params);
-        const response = (await requestURL(url, "json")) as SearchResponse;
-
-        if (response === null || response.results.length === 0) {
-            return items;
-        }
-
-        const app = createMarketApp(response.results[0]);
-        await upsertApp(app);
-
-        for await (const [index, result] of response.results.entries()) {
-            if ((await retrieveItemByName(result.name)) !== null) {
-                console.log(`[Search] Skipping item (${i + index + 1}/${itemCountLimit}) : ${result.name}`);
+        const ITEM_RETRIEVAL_DELAY = 20000;
+        for await (const [index, item] of itemEntries) {
+            if (storedItemsName.includes(item.name)) {
+                console.log(`[MarketFetch] Item '${item.name}' is already stored! (${i + index + 1}/${itemCount})`);
                 continue;
             }
 
-            console.log(`[Search] Fetching item (${i + index + 1}/${itemCountLimit}) : ${result.name}`);
-            const item = await createMarketItem(result);
-            items.push(item);
+            console.log(`[MarketFetch] Fetching item '${item.name}' from the Steam Market. (${i + index + 1}/${itemCount})`);
 
-            await insertItem(item);
+            const marketItem = await createMarketItem(item);
+            if (marketItem === null || marketItem.item_nameid === null) {
+                console.warn(
+                    `[MarketFetch] Failed to fetch Item '${item.name}' from Steam Market! (${i + index + 1}/${itemCount})`
+                );
+                await delay(ITEM_RETRIEVAL_DELAY);
+                continue;
+            }
+
+            const upsertedItem = await upsertItem(marketItem);
+            if (upsertItem === null) {
+                console.warn(`[MarketFetch] Item '${item.name}' failed to save in database! (${i + index + 1}/${itemCount})`);
+            }
+
             await delay(ITEM_RETRIEVAL_DELAY);
         }
-    }
 
-    return items;
-}
-
-async function getTotalItems(baseUrl: string, params: object) {
-    const url = urlcat(baseUrl, params);
-
-    try {
-        const response = (await requestURL(url, "json")) as SearchResponse;
-        return response.total_count;
-    } catch (error) {
-        return 0;
+        await delay(ITEM_RETRIEVAL_DELAY);
     }
 }
