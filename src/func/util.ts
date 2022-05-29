5f8ca3b6cd442f9b38b07028e67829e98fbce256
Dwi Andhika <53227252+siveroo@users.noreply.github.com>
2022-05-29 18:33:19 +0700

    improved fetch code

--- src/func/util.ts
+++ src/func/util.ts
@@ -5,16 +5,24 @@ export async function requestURL(url: string, type: "text" | "json", retryCount 
     let response: Response | null = null;
 
     for (let i = 0; i < MAX_RETRIES; i++) {
-        response = await fetch(url);
-
-        if (response.status === 429) {
-            console.warn(
-                `[Request RateLimit] Too much request when accessing '${type}' from '${url}' (${i + 1}/${MAX_RETRIES - 1})`
-            );
-            await delay(RETRY_DURATION);
-        } else {
-            break;
+        try {
+            response = await fetch(url);
+            if (response.status === 429) {
+                console.warn(
+                    `[Request RateLimit] Too much request when accessing '${type}' from '${url}' (${i + 1}/${MAX_RETRIES - 1})`
+                );
+            } else {
+                break;
+            }
+        } catch (err) {
+            if (err instanceof TypeError) {
+                console.error(`[${err.name}] ${err.message} (${i + 1}/${MAX_RETRIES - 1})`);
+            } else {
+                console.error(err);
+            }
         }
+
+        await delay(RETRY_DURATION);
     }
 
     if (response.ok) {
